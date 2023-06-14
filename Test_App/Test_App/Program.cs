18ec3413c50b5652f554880a6ad2e028632d96d6
Lachezar Shokolarev <lachezarshokolarev05@pmgkk.com>
2023-06-14 16:13:12 +0300

    Test commit 2

--- Test_App/Test_App/Program.cs
+++ Test_App/Test_App/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+namespace Test_App
+{
+    internal class Program
+    {
+        public static double Rect_Area(double width, double height)
+        {
+            return width * height;
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine(Rect_Area(rnd.Next(1,10), rnd.Next(1,10)));
+        }
+    }
+}
+
+
